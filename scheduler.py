3c4b8fa0977693f0af6575254e930d89cd118d3c
Bodybro <ivan.stereotekk@gmail.com>
2022-09-11 02:16:39 +0300

    scheduler: works! backend seems like done

---
+++ scheduler.py
@@ -0,0 +1,66 @@
+import schedule
+from curency import convert_rubles
+import traceback
+# DB Issues
+from models import Item
+from db import Session
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+# PARAMETERS FROM .env
+PARSING_PERIOD = os.getenv("PARSING_PERIOD")
+
+
+db_session = Session()
+
+
+def put_data_to_database():
+    """
+    Actually this is the periodic task, or function.
+    This function interacts with google sheet > takes data > put's data to database:
+    :return:
+    """
+    try:
+        from g_parser import get_all_records,list1
+        all_record = get_all_records(list1)
+        for row in all_record:
+            # Making new item from all records in google sheet:
+            new_Item = Item(
+                order_num=row['заказ №'],
+                price_usd=row['стоимость,$'],
+                rub_price=round(convert_rubles(row['стоимость,$']),2),
+                delivery_data=str(row['срок поставки']),
+            )
+            try:
+                same_Item = db_session.query(Item).filter(Item.order_num == new_Item.order_num).all()
+                print(same_Item[0],"same")
+            except:
+                try:
+                    db_session.add(new_Item)
+                    db_session.commit()
+                    print('commited new item')
+                except:
+                    db_session.rollback()
+                    db_session.close()
+        #renew
+        db_session.rollback()
+        db_session.close()
+        print("<<< DISCONNECTED DB:")
+        all_record = get_all_records(list1)
+    except (Exception,KeyboardInterrupt):
+        return f"******\nPeriodic Task Interupted By User >>>>:\n{traceback.format_exc()}"
+
+if __name__ == "__main__":
+    """
+    PARSING_PERIOD - seconds (.env )
+    RUN COMMAND: - $python scheduler.py
+    """
+    schedule.every(int(PARSING_PERIOD)).seconds.do(put_data_to_database)
+    while True:
+        schedule.run_pending()
+
+
